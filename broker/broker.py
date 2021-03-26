00c096dd3ca26339b1b96b58f4dcc62002c64495
Sami Laitinen <samipeeter.k@gmail.com>
2021-03-26 12:27:06 +0200

    First commit

---
+++ broker/broker.py
@@ -0,0 +1,98 @@
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import requests
+from urllib.parse import urlparse, parse_qs
+
+#########
+#VARIABLES:
+
+hostName = "0.0.0.0"
+serverPort = os.environ.get('PORT', 80)
+ServerURL = "http://server:79/"
+
+##########
+
+mutex = False
+latest_state = 0
+
+class MyServer(BaseHTTPRequestHandler):
+    def _set_get_response(self, board):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(json.dumps(board).encode('utf_8'))
+
+    def _set_post_response(self, status):
+        self.send_response(status)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        state = int(parse_qs(urlparse(self.path).query)["state"][0])
+        print("GET request received", self.path,"with params:", state,"From:", self.client_address)
+        board = get_board(state)
+        update_latest_state(board)
+        self._set_get_response(board)
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = json.loads(self.rfile.read(content_length))
+        
+        status = receive_post_from_client(post_data)
+        
+        self._set_post_response(status)
+	
+# Process request from client
+def receive_post_from_client(message):
+    global mutex
+    global latest_state
+    if authenticate_user():
+        print("Client state:", message["state"], "Latest state:", latest_state)
+        if message["state"] >= latest_state:
+            update_board(message)
+            return 200 #OK
+        else:
+            return 403 #Forbidden
+    else:
+        return 401 #Unauthorized
+        
+
+# Send request to server to get board status
+def get_board(state):
+    payload = {"state":state}
+    r = requests.get(url = ServerURL, params=payload)
+    print("GET returned:", r.text)
+    return r.text
+
+def update_board(message):
+    print("Sending post request to:", ServerURL)
+    r = requests.post(url = ServerURL, data=json.dumps(message))
+    global latest_state
+    latest_state += 1
+    return r.status_code
+
+def authenticate_user():
+    return True
+    
+def update_latest_state(board):
+    if not board:
+        global latest_state
+        board = json.loads(board)
+
+        if "is_whole_board" in board:
+            del board["is_whole_board"]
+
+        state = int(max(list(board.keys())))
+         
+        if state > latest_state:
+            latest_state = state
+
+if __name__ == "__main__":
+    webServer = HTTPServer((("", int(serverPort))), MyServer)
+    print("Broker started at http://%s:%s" % (hostName, serverPort))
+
+    webServer.serve_forever()
+
+    webServer.server_close()
+	
---
+++ server/server.py
@@ -0,0 +1,109 @@
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+from urllib.parse import urlparse, parse_qs
+#import logging
+
+PORT = os.environ.get('PORT', 79)
+DIMENSION = 50
+
+FORMAT = 'utf-8'
+
+MAX_STATES = 100
+EVAL = "BETTER"  # BAD or BETTER
+
+board = None
+
+
+class WhiteBoard:
+    def __init__(self):
+        self.pixels = [["WHITE"] * DIMENSION for i in range(DIMENSION)]
+        self.curState = 1
+        self.recentStates = {}
+
+    def changePixel(self, new_state):
+        try:
+            state, x, y, color = new_state.values()
+        except ValueError:
+            print("Incorrect new state format")
+            return 409 # BAD REQUEST
+        self.pixels[x][y] = color
+        # Check dictionary for MAX_STATES and remove the oldest entry if needed
+        stateKeys = list(self.recentStates.keys())
+        if self.curState > MAX_STATES:
+            self.recentStates.pop(stateKeys[0])
+        self.curState = self.curState + 1
+        self.recentStates[str(self.curState)] = {"x": x, "y": y, "color": color}
+        print(f"State:{self.curState}, POST: ({x},{y}): {color}")
+        return 200 # Succeed, OK
+
+
+    def getPixels(self, state):
+        # Return type depending on evaluation method
+        print("GET:", state)
+        print("Latest state:",self.curState)
+        if EVAL == "BAD":
+            return {str(self.curState):self.pixels, "is_whole_board":True}
+        if EVAL == "BETTER":
+            # if client is new or it is too far behind
+            # --> return board
+            # else return most recent states client current to the latest
+            if (state == 0) or ((self.curState - state) > MAX_STATES): 
+                print("return board")
+                return {str(self.curState):self.pixels, "is_whole_board":True}
+            elif self.curState == 1 or state == self.curState: # empty board or client state is curState
+                return {}
+            else: 
+                stop = self.curState
+                print(f"state:{state} - stop:{stop}")
+                subset = {str(key): self.recentStates[str(key)] for key in list(range(state+1,stop+1))}
+                print("return", subset)
+                return subset
+
+class myServer(BaseHTTPRequestHandler):
+    def _set_get_response(self):
+        self.send_response(200)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+
+    def _set_post_response(self, status):
+        self.send_response(status)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+
+
+    def do_GET(self):
+        from sys import getsizeof
+        state = int(parse_qs(urlparse(self.path).query)["state"][0])
+        #logging.info("")
+        self._set_get_response()
+        global board
+        pixels = getsizeof(json.dumps(board.getPixels(state)))
+        encoded = getsizeof(json.dumps(board.getPixels(state)).encode(FORMAT))
+        self.wfile.write(json.dumps(board.getPixels(state)).encode(FORMAT))
+
+    def do_POST(self):
+        content_length = int(self.headers['content-length'])
+        post_data = json.loads(self.rfile.read(content_length))
+        #logging.info("")
+        global board
+        res = board.changePixel(post_data)
+        self._set_post_response(res)
+
+def main():
+    global board
+    board = WhiteBoard()
+    httpd = HTTPServer(("", int(PORT)), myServer)
+    print(f"Server running on port {PORT} with {DIMENSION}x{DIMENSION} board")
+    #logging.info('Starting server...\n')
+    #setattr(httpd, 'board', WhiteBoard())
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+       pass
+    httpd.server_close()
+    #logging.info('Stopping server...\n')
+
+
+if __name__ == '__main__':
+    main()
